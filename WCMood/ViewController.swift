c7b4ed2a6066c079ee4de0b20ff225789ca0028e
nur <mirrorvol@gmail.com>
2024-05-13 12:26:30 +0400

    Initial Commit

---
+++ WCMood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WCMood
+//
+//  Created by mix on 13.5.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
